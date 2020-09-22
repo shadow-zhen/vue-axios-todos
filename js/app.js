d8b4a6eda5331cd08eb3d318333f63019d72a02d
shadow-zhen <1378565280@qq.com>
2020-09-22 22:35:15 +0800

    基于async和await的ajax请求完成

--- js/app.js
+++ js/app.js
@@ -13,67 +13,38 @@
     },
     methods: {
       //渲染页面发送ajax请求
-      render () {
-        axios({
-          method: 'get',
-          //_sort：以什么排序，_order：降序（desc）还是升序（asc）
-          url: 'http://localhost:3000/todos?_sort=id&_order=desc'
-        }).then(
-          res => {
-            // console.log(res)
-            this.todos = res.data
-          }
-        )
+      async render () {
+        const res = await axios.get('http://localhost:3000/todos?_sort=id&_order=desc')
+        this.todos = res.data
       },
       //添加一项todo
-      addTodo () {
-        axios({
-          method: 'post',
-          url: 'http://localhost:3000/todos',
-          data: {
-            todoName: this.todoName,
-            state: false
-          }
-        }).then(res => {
-          this.render()
-          this.todoName = ''
+      async addTodo () {
+        await axios.post('http://localhost:3000/todos', {
+          todoName: this.todoName,
+          state: false
         })
+        this.render()
+        this.todoName = ''
       },
       //删除todo
-      delTodo (id) {
-        axios({
-          method: 'delete',
-          url: `http://localhost:3000/todos/${id}`
-        }).then(res => this.render())
+      async delTodo (id) {
+        await axios.delete(`http://localhost:3000/todos/${id}`)
+        this.render()
       },
       //点击前面的按钮，修改状态
-      changeSate (id, state) {
-        axios({
-          method: 'patch',
-          url: `http://localhost:3000/todos/${id}`,
-          data: { state }
-        }).then(
-          res => {
-            this.render()
-          }
-        )
+      async changeSate (id, state) {
+        await axios.patch(`http://localhost:3000/todos/${id}`, { state })
+        this.render()
       },
       //展示修改框
       showInp (id) {
         this.now = id
       },
       //按enterr键，修改todoName
-      changeTodoName (id, todoName) {
-        axios({
-          method: 'patch',
-          url: `http://localhost:3000/todos/${id}`,
-          data: { todoName }
-        }).then(
-          res => {
-            this.now = -1
-            this.render()
-          }
-        )
+      async changeTodoName (id, todoName) {
+        await axios.patch(`http://localhost:3000/todos/${id}`, { todoName })
+        this.render()
+        this.now = -1
       }
     },
     computed: {
